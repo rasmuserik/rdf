e9038d68315140f1add52fe3b6c475c82c0e46b8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-09-23 22:47:33 +0200

    load data from natmus

--- src/solsort/rdf/rdf.cljs
+++ src/solsort/rdf/rdf.cljs
@@ -15,6 +15,9 @@
    [clojure.string :as string :refer [replace split blank?]]
    [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
 
+(js/console.log (.-globalPaths (js/require "module")))
+(js/console.log (str (js/process.cwd) "/node_modules"))
+(.push (.-globalPaths (js/require "module")) (str (js/process.cwd) "/node_modules"))
 (def process js/process)
 (def require js/require)
 (def http (require "http"))
@@ -24,8 +27,60 @@
   (.setHeader res "type" "text/html")
   (.end res (reagent/render-to-static-markup [:h1 "Hello from cljs - docker"])))
 
+(defn natmus-id [obj]
+  (str "natmus:" (get obj "collection") ":" (get obj "sourceId")))
+  (defn obj-ref "return full url for id"
+    [id]
+    (str
+    ; "http://rdf.solsort.com"
+         "/object/" id)
+    )
+(defn natmus-title [obj]
+  (get obj "workDescription"))
+(defn <natmus-search [q limit from]
+  (go
+    (->
+     (<! (<ajax (str "http://samlinger.natmus.dk/api/all/_search"
+                     "?q=" q
+                     "&from=" from
+                     "&size=" limit)
+                :credentials false
+                :type :json))
+     (get "hits")
+     (get "hits")
+     (->> (map #(get % "_source"))))))
+(defn show-object [req res]
+  ; collection:DO AND sourceId:1234
+  (.end res (str "show-object" (prn-str (.-params req)))))
+(defn natmus-search [req res]
+  (go
+    (let [search-results (<! (<natmus-search (.-query (.-params req)) 100 0))]
+      )
+    (.end res (reagent/render-to-static-markup
+               [:html
+                [:head
+                 [:meta {:charset "utf-8"}]]
+                [:body(into [:ul]
+                 (map (fn [obj]
+                        [:li [:a {:href (obj-ref (natmus-id obj))}
+                              (natmus-title obj)]])
+                      (<! (<natmus-search
+                           (.-query (.-params req))
+                           100 0))))]]))))
+;(natmus-search {} #js{:params #js{:query "hest"}})
 (defonce server
-  (.listen (.createServer http
-                  #(handler %1 %2)) 8888 #(js/console.log "listening")))
+  (let [express (require "express")
+        app (express)]
+    (.get app "/object/:id.:type" #(show-object %1 %2))
+    (.get app "/search/natmus/:query" #(natmus-search %1 %2))
+    (.listen app 8000 #(console.log "started express on port 8000"))
+    #_(.listen
+     (.createServer
+      http
+      #(handler %1 %2)) 8888 #(js/console.log "listening"))))
 
-(js/console.log process)
+
+#_(go
+  (js/console.log (clj->js (<! (<ajax "http://samlinger.natmus.dk/api/all/_search?q=hest" :credentials false))))
+  (js/console.log (clj->js (<! (<ajax "http://samlinger.natmus.dk/api/assets/_mapping" :credentials false))))
+  )
